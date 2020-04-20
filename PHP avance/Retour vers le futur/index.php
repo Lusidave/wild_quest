ee755c50b2ae378b1a878028b077197e1c478691
Lusidave <lusidave@hotmail.fr>
2020-04-20 19:44:45 +0200

    Bonus en plus

--- PHP avance/Retour vers le futur/index.php
+++ PHP avance/Retour vers le futur/index.php
@@ -26,6 +26,8 @@ $presentTime = $presentTime->format('M-d-Y - A h:i');
 
 // Arrivée
 $destinationTime = (new DateTime())->setDate('2040', '02', '13');
+$destinationTime = $destinationTime->setTime('14', '47');
+//$destinationTime = $destinationTime->format('M-d-Y' - 'A h:i');
 
 $Aday = date_format($destinationTime, 'd');
 $Amonth = date_format($destinationTime, 'M');
@@ -36,13 +38,26 @@ $Ahour = date_format($destinationTime, 'h');
 $Amin = date_format($destinationTime, 'i');
 $ApresentTime = $destinationTime->format('M-d-Y - A h:i');
 
-$destinationTime = $destinationTime->format('M-d-Y - A h:i');
-
-
+// 3eme
 $datetime1 = new DateTime();
 clone($datetime1);
 $datetime2 = (new DateTime())->setDate('2040', '02', '13');
+$datetime2 = $datetime2->setTime(12, 54);
 $interval = $datetime1->diff($datetime2);
+// Calcul en minutes
+$days1 = $interval->days * 24 * 60;
+$hour1 = $interval->h * 60;
+$min1 = $interval->i;
+$result = $days1 + $hour1 + $min1;
+
+// Calcul pour carburant
+$result2 = $result / 10000;
+
+$datetime3 = new DateTime();
+clone($datetime3);
+$datetime4 = (new DateTime())->setDate('2040', '02', '13');
+$datetime4 = $datetime4->setTime(12, 54);
+$interval = $datetime3->diff($datetime4);
 ?>
 
 
@@ -51,7 +66,7 @@ $interval = $datetime1->diff($datetime2);
 <div class="container">
     <div class="row">
         <p class="col-12">Date de départ : <?php echo $presentTime?></p>
-        <p class="col-12">Date d'arrivée : <?php echo $destinationTime?></p>
+        <p class="col-12">Date d'arrivée : <?php echo $ApresentTime?></p>
     </div>
 
 
@@ -134,6 +149,12 @@ $interval = $datetime1->diff($datetime2);
 <div class="container">
 <p>Intervalle entre les deux dates :</p>
 <p><?php echo $interval->format('%Y année %m mois, %d jour %h heure %i minute');?></p>
+
+    <p> Intervalle entre les deux dates en minutes et secondes :</p>
+    <p><?php echo $result . ' minutes';?></p>
+
+    <p> Le nombre de Litres de carburant nécessaires pour le trajet est de :</p>
+    <p><?php echo $result2 . ' Litres ';?></p>
 </div>
 
 
